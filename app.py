29a07c6d3d33f4f99f6843b479113877f49caf4a
Tapan Karangiya <olofury@gmail.com>
2024-06-24 18:25:53 +0530

    upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+# Load your trained model
+model = tf.keras.models.load_model('/Users/tapankarangiya/Desktop/WORK/plant/PlantVillage/trained.keras')
+
+# Define class names
+class_names = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy'] # Replace with your actual class names
+
+def predict(model, img):
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+
+    predictions = model.predict(img_array)
+
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    return predicted_class, confidence
+
+# Streamlit app
+st.title('Potato Disease Classification')
+
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+
+    predicted_class, confidence = predict(model, image)
+
+    st.write(f"Predicted Class: {predicted_class}")
+    st.write(f"Confidence: {confidence}%")
